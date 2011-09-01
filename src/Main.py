108f4f9aa46710cf1fa7b0b935735a194e578168
AClockWorkLemon <info.clockworkgames@gmail.com>
2011-09-01 12:55:55 +1000

    Just setting up a bit of a package system

---
+++ src/Main.py
@@ -0,0 +1,8 @@
+'''
+Ok, lets get this thing started.
+Can i suggest we have the main IRC Protocl in the main package (here), and the site polling/control panel/module system/blah in separate packages?
+I'll start putting together the module system, ill put some more details in here on how to call it later :)
+'''
+
+if __name__ == '__main__':
+    pass
