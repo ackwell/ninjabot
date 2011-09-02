26ceca0d64b1256b77102d91a9aad8aaad4b361c
AClockWorkLemon <info.clockworkgames@gmail.com>
2011-09-02 22:23:30 +1000

    Added credentials.py example, moved goo.gl URL to Credentials.py (going to be using this as an API settings file from now on, might rename to reflect this later)

---
+++ src/apis/CredentialsExample.py
@@ -0,0 +1,11 @@
+#Rename this file to "Credentials.py" and edit variables below to your API settings
+
+
+PASTEBIN = {
+"api_key":"pastebin API key here"
+}
+
+GOOGL = {
+"url":"https://www.googleapis.com/urlshortener/v1/url",
+"api_key":"goo.gl API key here"
+}
--- src/apis/GooGl.py
+++ src/apis/GooGl.py
@@ -3,15 +3,13 @@ from urllib2 import Request, urlopen
 from urllib import urlencode
 import json
 
-url="https://www.googleapis.com/urlshortener/v1/url"
-
 def get_short(longUrl):
     data = {
             "longUrl":longUrl,
             "key":GOOGL["api_key"]
             }
     jdata = json.dumps(data)
-    req = Request(url,jdata,{'content-type': 'application/json'})
+    req = Request(GOOGL["url"],jdata,{'content-type': 'application/json'})
     u = urlopen(req).read()
     return json.loads(u)["id"]
 
@@ -20,5 +18,5 @@ def get_long(shortUrl):
             "shortUrl":shortUrl,
             "key":GOOGL["api_key"]
             }
-    u = urlopen(url+"?"+urlencode(args)).read()
+    u = urlopen(GOOGL["url"]+"?"+urlencode(args)).read()
     return json.loads(u)["longUrl"]
