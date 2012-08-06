51b014b823ccc23ec00d5618064a2c69f1433dae
Saxon Landers <aclockworklemon1@gmail.com>
2012-08-06 16:32:47 +1000

    Fixed CTCP derp

--- src/Plugins/ctcp.py
+++ src/Plugins/ctcp.py
@@ -3,24 +3,27 @@ SOURCE = 'SOURCE https://github.com/AClockWorkLemon/NCSSBot'
 
 class Plugin:
 	def __init__(self, controller):
-		self.controller = controller
+		self.c = controller
 
 	def on_incoming(self, msg):
 		if not msg.ctcp: return msg
+
+		print msg.ctcp
 
 		args = msg.ctcp.split()
 		command = args.pop(0).lower()
 
 		if command == 'version':
+			self.c.notice(msg.nick, VERSION+('GUI' if self.c.gui.graphical else 'CLI')+' Mode')
 			msg.body += 'Recieved CTCP VERSION from '+msg.nick
 			msg.nick = '*'
 			msg.ctcp = ''
-			self.controller.notice(msg.nick, VERSION+('GUI' if self.controller.gui.graphical else 'CLI')+' Mode')
+		
 		elif command == 'source':
+			self.c.notice(msg.nick, SOURCE)
 			msg.body += 'Recieved CTCP SOURCE from '+msg.nick
 			msg.nick = '*'
 			msg.ctcp = ''
-			self.controller.notice(msg.nick, SOURCE)
 
 		elif command == 'action':
 			msg.body = msg.nick+' '+' '.join(args)
