54eeb331a2b5839787179d5d96c00994c5e089eb
George Caley <george@caley.com.au>
2012-08-10 15:22:18 +1000

    Added support for regex in past messages, and fixed backslash escaping.

--- src/Plugins/regex.py
+++ src/Plugins/regex.py
@@ -1,44 +1,58 @@
 # Regex Plugin for NCSSBot
 
+from collections import defaultdict, deque
 import re
 
-last_messages = dict()
+BACKLOG = 3 # keep this many past messages saved for each user
+
+last_messages = defaultdict(deque)
 
 class Plugin:
     def __init__(self, controller):
         self.controller = controller
-    
+
     def on_incoming(self, msg):
         if msg.type == msg.CHANNEL:
             # check if the message matches the s/blah/blah/ syntax
             # regex could have been used for this, but factoring in those escaped forward slashes
             # would have been more trouble than it was worth...
             body = msg.body
+            their_messages = last_messages[msg.nick]
+
             groups = list()
             current_group = ''
             for i in range(len(body)):
                 if i == 0 and body[i] != 's':
-                	break
+                    break
                 elif i == 1 and body[i] != '/':
                     break
-                elif body[i] == '/' and body[i-1] != '\\':
+                elif body[i] == '/' and (body[i-1] != '\\' or (len(body) >= 3 and body[i-2] == '\\')):
                     groups.append(current_group)
                     current_group = ''
                 else:
-                	current_group += body[i]
+                    current_group += body[i]
             else:
-            	if len(current_group) == 0 and len(groups) == 3:
+                if len(current_group) == 0 and len(groups) == 3:
                     # did they have a last message?
                     if msg.nick in last_messages:
-                        last_message = last_messages[msg.nick]
-
                         _, pattern, replacement = map(lambda s: s.replace('\\/', '/'), groups)
 
-                        # perform regex
-                        body = re.sub(pattern, replacement, last_message)
-                        
-                        # send it
-                        self.controller.privmsg(msg.channel, '%s meant to say: %s' % (msg.nick, body))
+                        # scan for a matching message in their last messages
+                        for message in their_messages:
+                            if re.search(pattern, message):
+                                body = re.sub(pattern, replacement, message)
+
+                                # send it
+                                self.controller.privmsg(msg.channel, '%s meant to say: %s' % (msg.nick, body))
+
+                                break
+                        else:
+                        	# match wasn't found
+                        	# return without adding this to their last messages
+                        	return
 
             # add it to the last messages dictionary
-            last_messages[msg.nick] = body
+            their_messages = last_messages[msg.nick]
+            their_messages.appendleft(body)
+            if len(their_messages) > BACKLOG:
+                their_messages.pop()
