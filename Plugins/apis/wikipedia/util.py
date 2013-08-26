9bcaab6ef907ca0a467ed638ca6594a2126cfece
Peter Arnott <peterarenot@gmail.com>
2013-08-26 20:01:16 +1000

    Added the wikipedia plugin

---
+++ Plugins/apis/wikipedia/util.py
@@ -0,0 +1,25 @@
+import functools
+
+def debug(fn):
+	def wrapper(*args, **kwargs):
+		print fn.__name__, "called with args:\n", str(args) + str(kwargs), "\n"
+		res = fn(*args, **kwargs)
+		print "\nreturning:", res, "\n\n"
+		return res
+	return wrapper
+
+def cache(fn):
+
+	_cache = {}
+
+	@functools.wraps(fn)
+	def wrapper(*args, **kwargs):
+		key = str(args) + str(kwargs)
+		if key in _cache:
+			ret = _cache[key]
+		else:
+			ret = _cache[key] = fn(*args, **kwargs)
+
+		return ret
+
+	return wrapper
