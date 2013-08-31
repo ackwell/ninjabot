7b69dcc272bf676fada468fa1537218564f4a23c
Nick Armstrong <nick@nickarmstrong.id.au>
2013-08-31 11:55:50 +1000

    Tried to fix htmlentities because wut. Gave up

--- Plugins/apis/htmlentities.py
+++ Plugins/apis/htmlentities.py
@@ -1,16 +1,17 @@
-
 import re
 from htmlentitydefs import entitydefs as ENTITIES
 
-fix_charcodes = lambda code: "\u" . hex(int(code.groups()[0]))
-fix_entities  = lambda name: ENTITIES[name.groups()[0]] if name.groups()[0] in ENTITIES else u"\u000304" + name.groups()[0] + u"\u000f" # red!
+def fix_charcodes(code):
+    return unichr(int(code.groups()[0]))
 
+def fix_entities(name):
+    return ENTITIES.get(name.groups()[0], u"\u000304" + name.groups()[0] + u"\u000f")
 
 def clear_entities(text):
-	#print u"Entity-freeing %s"%unicode(text)
-	text = re.sub("&amp;", "&", text);
-	text = re.sub("&(\w+?);", fix_entities, text);
-	text = re.sub("&#(\d\d\d);", fix_charcodes, text);
-	return text
+    #print u"Entity-freeing %s"%unicode(text)
+    text = unicode(text)
+    text = re.sub(r'&(\w+);',  fix_entities,  text);
+    text = re.sub(r'&#(\d+);', fix_charcodes, text);
+    return text
 
-	
+    
