5ef5a842f6b4a441c91aa15d20028e29450248dc
ackwell <saxon@ackwell.com.au>
2013-09-19 22:44:45 +1000

    ported core.git

---
+++ plugins/core/git.py
@@ -0,0 +1,18 @@
+# git plugin
+# allows 'git pull' to be run remotely
+
+import time
+
+class Plugin:
+    def __init__(self, bot, config):
+        self.bot = bot
+        self.git = self.bot.request_api('git').Git()
+
+    def trigger_gitpull(self, msg):
+        if self.bot.is_admin(msg.nick):
+            self.bot.notice(msg.nick, 'Running git pull...')
+            response = self.git.pull()
+            for line in response.split('\n'):
+                self.bot.notice(msg.nick, line)
+                time.sleep(0.2)
+            self.bot.notice(msg.nick, 'Done!')
