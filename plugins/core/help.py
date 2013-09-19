f322063f5e30b88c6e22ba91ccae7f8149411f35
ackwell <saxon@ackwell.com.au>
2013-09-19 22:48:17 +1000

    Ported core.help

---
+++ plugins/core/help.py
@@ -0,0 +1,26 @@
+class Plugin:
+	def __init__(self, bot, config):
+		self.bot = bot
+
+	def trigger_help(self, msg):
+		"Usage: help [trigger]. Lists avaliable triggers. If trigger is specified, will print help for that trigger (if avaliable)."
+
+		prefix = self.bot.command_prefix
+		if len(msg.args) == 0:
+			self.bot.notice(msg.nick, 'Avaliable triggers:')
+			self.bot.notice(msg.nick, prefix+(', '+prefix).join(sorted(self.bot.triggers.keys())))
+			self.bot.notice(msg.nick, 'For further info, type {0}help <trigger>'.format(prefix))
+		else:
+			if msg.args[0].lstrip(prefix) in self.bot.triggers:
+				doc = self.bot.triggers[msg.args[0].lstrip(prefix)].__doc__
+				if doc:
+					self.bot.notice(msg.nick, doc)
+				else:
+					self.non_existent_help(msg)
+			else:
+				self.non_existent_help(msg)
+
+	def non_existent_help(self, msg):
+		self.bot.notice(msg.nick, "Either that trigger does not exist, or it has no documentation.")
+		
+
