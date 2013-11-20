4dec5b5f28359920df8e577663eeace84c447204
Dominic May <jack.thatch@gmail.com>
2013-11-20 09:49:05 +0800

    Mended Storage api
    It is not a good idea to pickle.dump a live class

--- apis/storage.py
+++ apis/storage.py
@@ -3,19 +3,20 @@
 import os
 import pickle
 import warnings
+import collections
 
 
-class Storage(dict):
+class Storage(collections.UserDict):
 	def __init__(self, plugin, bot):
-		# Set the filename based on the plugin name
-		self._fname = '.'.join(plugin.__module__.split('.')[1:])
-		self._base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
-		self._config = bot.config
+		# initiate the UserDict
+		super().__init__()
 
-		if self._config.get('storage', {}).get('path', False):
-			self._path = self._config['storage']['path']
-		else:
-			self._path = 'storage'
+		# Set the filename based on the plugin name
+		self._fname = plugin.__module__.partition('.')[-1]
+		self._base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+
+		self._config = bot.config.get('storage', {})
+		self._path = self._config.get('path', 'storage')
 
 		self._path = os.path.join(self._base_path, self._path)
 		self._full_path = os.path.normpath(os.path.join(self._path, self._fname))
@@ -25,9 +26,9 @@ class Storage(dict):
 
 		if os.path.exists(self._full_path):
 			with open(self._full_path, 'rb') as fh:
-				self.update(pickle.load(fh))
+				self.data.update(pickle.load(fh))
 
-		if not self._config.get('storage', {}).get('alwayswrite', False):
+		if not self._config.get('alwayswrite', False):
 			bot.register_storage(self)
 
 	def __repr__(self):
@@ -37,9 +38,9 @@ class Storage(dict):
 		warnings.warn('This method is depreciated; just use the storage object directly')
 		self[key] = value
 
-	def __setitem__(self, *args, **kwargs):
-		super().__setitem__(*args, **kwargs)
-		if self._config.get('storage', {}).get('alwayswrite', False):
+	def __setitem__(self, key, value):
+		super().__setitem__(key, value)
+		if self._config.get('alwayswrite', False):
 			self.write()
 
 	def get_dict(self):
@@ -48,4 +49,4 @@ class Storage(dict):
 
 	def write(self):
 		with open(self._full_path, 'wb') as fh:
-			pickle.dump(self, fh)
+			pickle.dump(self.data, fh)
